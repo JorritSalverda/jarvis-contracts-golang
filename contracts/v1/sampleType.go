01b7674f75144e1da509fae257ec2e41295f94a6
Jorrit Salverda <jsalverda@travix.com>
2020-10-07 10:36:19 +0200

    drop unit, it's tied to the sample type; rename aggregated level to entity type

--- contracts/v1/sampleType.go
+++ contracts/v1/sampleType.go
@@ -3,12 +3,13 @@ package contracts
 type SampleType string
 
 const (
-	SampleType_SAMPLE_TYPE_INVALID     SampleType = ""
-	SampleType_SAMPLE_TYPE_ELECTRICITY SampleType = "SAMPLE_TYPE_ELECTRICITY"
-	SampleType_SAMPLE_TYPE_GAS         SampleType = "SAMPLE_TYPE_GAS"
-	SampleType_SAMPLE_TYPE_TEMPERATURE SampleType = "SAMPLE_TYPE_TEMPERATURE"
-	SampleType_SAMPLE_TYPE_PRESSURE    SampleType = "SAMPLE_TYPE_PRESSURE"
-	SampleType_SAMPLE_TYPE_FLOW        SampleType = "SAMPLE_TYPE_FLOW"
-	SampleType_SAMPLE_TYPE_HUMIDITY    SampleType = "SAMPLE_TYPE_HUMIDITY"
-	SampleType_SAMPLE_TYPE_TIME        SampleType = "SAMPLE_TYPE_TIME"
+	SampleType_SAMPLE_TYPE_INVALID                 SampleType = ""
+	SampleType_SAMPLE_TYPE_ELECTRICITY_CONSUMPTION SampleType = "SAMPLE_TYPE_ELECTRICITY_CONSUMPTION"
+	SampleType_SAMPLE_TYPE_ELECTRICITY_PRODUCTION  SampleType = "SAMPLE_TYPE_ELECTRICITY_PRODUCTION"
+	SampleType_SAMPLE_TYPE_GAS                     SampleType = "SAMPLE_TYPE_GAS"
+	SampleType_SAMPLE_TYPE_TEMPERATURE             SampleType = "SAMPLE_TYPE_TEMPERATURE"
+	SampleType_SAMPLE_TYPE_PRESSURE                SampleType = "SAMPLE_TYPE_PRESSURE"
+	SampleType_SAMPLE_TYPE_FLOW                    SampleType = "SAMPLE_TYPE_FLOW"
+	SampleType_SAMPLE_TYPE_HUMIDITY                SampleType = "SAMPLE_TYPE_HUMIDITY"
+	SampleType_SAMPLE_TYPE_TIME                    SampleType = "SAMPLE_TYPE_TIME"
 )
